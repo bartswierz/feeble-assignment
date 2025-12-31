8e7d1b12bcee486bc5f4fec70340d335b3031611
bartswierz <bartoszswierzynski7@gmail.com>
2025-12-31 11:28:20 -0600

    added cloud & bird positioning - WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,37 @@
 import Navigation from "./components/Navigation/Navigation";
 import Hero from "./components/Hero/Hero";
-import Birds from "./assets/backgrounds/birds.svg";
-import Clouds from "./assets/backgrounds/clouds.svg";
 import CallToAction from "./components/CallToAction/CallToAction";
+import Birds from "./assets/backgrounds/birds.svg";
+import SingleBird from "./assets/backgrounds/single-bird.svg";
+import Clouds from "./assets/backgrounds/clouds.svg";
 
 function App() {
   return (
     <div className="mx-auto max-w-[1920px] w-full overflow-hidden relative h-full">
       <Navigation />
-
-      {/* Hero region needs to be relative */}
-      {/* <div> */}
       <Hero />
 
-      {/* Background layers anchored to 1920 container */}
-      <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
-        {/* Birds (HeroBg) */}
-        <img
-          src={Birds}
-          alt="Birds Flying"
-          aria-hidden="true"
-          className="
+      {/* Bird #1 - top 329px / 495px left */}
+      <img src={SingleBird} alt="Single Bird Flying" aria-hidden="true" className="absolute top-[329px] left-[495px]" />
+      {/* Bird #2 - top 465px / 1446px left */}
+      <img src={SingleBird} alt="Single Bird Flying" aria-hidden="true" className="absolute top-[465px] left-[1446px]" />
+
+      {/* Bird Group */}
+      <img
+        src={Birds}
+        alt="Birds Flying"
+        aria-hidden="true"
+        className="
               absolute
               w-[1407.935px] h-[719.378px]
               top-[291px] left-[298.18px]
               max-w-none
               transition-transform duration-900 ease-out
-
             "
-        />
+      />
 
-        {/* Clouds (bottom) */}
-        <img
-          src={Clouds}
-          alt="Clouds"
-          className="
-              absolute bottom-0 left-1/2
-              w-full max-w-[1920px] z-1
-            "
-        />
+      <div className="border-2 max-w-[1920px] h-full">
+        <img src={Clouds} alt="Clouds" className="w-full max-w-[1920px]" />
       </div>
       <CallToAction />
     </div>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,10 +4,10 @@ import TextBubble from "../../assets/icons/text-bubble.svg";
 const Hero = () => {
   return (
     <div className="max-w-[736px] w-full mx-auto flex flex-col items-center gap-8 mt-[44px]">
-      <div className="flex flex-col text-center items-center gap-6">
-        <div className="flex items-center pt-1 px-2 pb-[5px] text-brand-blue font-medium text-[16px]">
+      <div className="flex flex-col text-center items-center gap-6 max-w-[90vw] mb-12">
+        <div className="flex items-center pt-1 px-2 pb-[5px] font-medium text-[16px] text-brand-blue bg-white rounded-full">
           <img src={TextBubble} alt="Text Icon" className="inline-block mr-2" />
-          #1 IMessage Automation Tool
+          #1 iMessage Automation Tool
         </div>
 
         <div className="font-bold text-[64px] leading-[120%]">
@@ -21,15 +21,15 @@ const Hero = () => {
       </div>
 
       {/* CTA Buttons */}
-      <div className="flex gap-2">
+      <div className="flex gap-2 mb-[54px]">
         <button className="flex max-w-[136px] w-full py-3 px-5 bg-brand-blue text-white cursor-pointer rounded-full items-center justify-center">
           Get Started
         </button>
 
         <button className="flex border-[#6C788F] border-[0.7px] rounded-full py-3 pl-5 pr-8 items-center gap-3 cursor-pointer">
           <img src={Apple} alt="Apple Icon" />
-          <span className="h-[18px] border-[0.5px] bg-[#6C788F]"></span>
-          <span className="text-[18px] font-medium whitespace-nowrap">Download the Mac app</span>
+          <div className="h-[18px] p-[0.5px] bg-[#6C788F]" />
+          <span className="whitespace-nowrap">Download the Mac app</span>
         </button>
       </div>
     </div>
